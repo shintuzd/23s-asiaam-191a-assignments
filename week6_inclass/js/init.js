b722f7fc304cb88d9b9b9bc1effc757e05ff412a
shintuzd <102273022+shintuzd@users.noreply.github.com>
2023-05-11 18:10:54 -0700

    fixing week5 survey size

---
+++ week6_inclass/js/init.js
@@ -0,0 +1,36 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNq8_prhrSwK3CnY2pPptqMyGvc23Ckc5MCuGMMKljW-dDy6yq6j7XAT4m6GG69CISbD6kfBF0-ypS/pub?output=csv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data.lat,data.lng,data['Where did you get vaccinated?'],data['Have you been vaccinated?'])
+    })
+}
+
+loadData(dataUrl)
