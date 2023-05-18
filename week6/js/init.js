acb34fea3dd5fcd9176f3cd55d5cc5c9ec5ae20e
shintuzd <102273022+shintuzd@users.noreply.github.com>
2023-05-17 21:45:40 -0700

    efficiency

--- week6/js/init.js
+++ week6/js/init.js
@@ -1,7 +1,8 @@
 // Mapping Favorite Restaurants Map
 
 // Survey Spreadsheet Dataset
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3JMLOzEGJEKn7YFIig9PCzerJO5TofP09ThGtSsqt41yRbZDP9BlPrFSyLAjEKR4cmkIoR2N5MYbR/pub?output=csv"
+const surveyUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3JMLOzEGJEKn7YFIig9PCzerJO5TofP09ThGtSsqt41yRbZDP9BlPrFSyLAjEKR4cmkIoR2N5MYbR/pub?output=csv"
+const placesUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRkmSXxg8YWhtkeX7iiBcY-uYjHURNjBdMz9iNUGyUIekHd2Jfp1CqxVRbvwHt-uJZF0ecZWctmHCim/pub?output=csv"
 
 // JSON for the Map Center and Zoom
 let mapOptions3 = {'center': [34.05486654874919, -118.26360364555003],'zoom':10}
@@ -29,35 +30,46 @@ function createButtons2(lat,lng,restaurant_name){
     document.getElementById("buttons2").appendChild(newButton); //this adds the button to our page.
   }
 
-// Adding markers and buttons
-function addMarker3(data){
-    console.log(data["What's the name of your favorite restaurant?"])
-    L.marker([data.lat,data.lng]).addTo(map3).bindPopup(`<h2>${data["What's the name of your favorite restaurant?"]}</h2> <h3>Submitted by: ${data["What's your name?"]}</h3> <p>What I Like About It: ${data["What do you like about it?"]}</p>`)
-    createButtons2(data.lat,data.lng,data["What's the name of your favorite restaurant?"])
-    return data["What's the name of your favorite restaurant?"]
+function addMarker(data, target_map=1){
+    if (target_map == 3){
+        L.marker([data.lat,data.lng]).addTo(map3).bindPopup(`<h2>${data["What's the name of your favorite restaurant?"]}</h2> <h3>Submitted by: ${data["What's your name?"]}</h3> <p>What I Like About It: ${data["What do you like about it?"]}</p>`)
+        createButtons2(data.lat,data.lng,data["What's the name of your favorite restaurant?"])
+        return data["What's the name of your favorite restaurant?"]
+    }
+    else {
+        L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.city}</h2> <h3>${data.description}</h3>`)
+    }
 }
 
+
 // Loading in the data
-function loadData(url){
+function loadData(url, tgt_map){
     Papa.parse(url, {
         header: true,
         download: true,
-        complete: results => processData(results)
+        complete: results => processData(results,tgt_map)
     })
 }
 
 // Processing the data into the map
-function processData(results){
+function processData(results, tgt_map){
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
-        addMarker3(data)
+        if (tgt_map ==3){
+            addMarker(data, 3)
+        }
+        else{
+            addMarker(data)
+        }
+        
     })
 }
 
 // this is our function call to get the data
 // we will put this comment to remember to call our function later!
-loadData(dataUrl)
+loadData(surveyUrl, 3)
+loadData(placesUrl, 1)
 
 // Places I've worked map
 
@@ -119,25 +131,3 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //JavaScript let variable declaration to create a marker
-
-function addMarker(lat,lng,title,message){
-    console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
-    return message
-}
-
-addMarker(34.052235,-118.243683,'Los Angeles','Where I live!')
-addMarker(37.774929,-122.419418,'San Francisco','First trip by myself! I went to visit some friends in the Bay Area.')
-addMarker(32.715736,-117.161087,'San Diego','Been here once. It\'s okay.')
-addMarker(48.856613,2.352222,'Paris','Came here during my trip to Europe. Beautiful city!')
-addMarker(51.507351,-0.127758,'London','The weathers always gloomy. Felt like an English New York.')
-addMarker(40.416775,-3.703790,'Madrid','The people in Madrid were so chill.')
-addMarker(41.385063,2.173404,'Barcelona','It was chill here, but not as chill as Madrid. Still a nice city, except the part where they have a huge Christopher Columbus memorial.')
-addMarker(50.822529,-0.137163,'Brighton','The people were so friendly here. Theres also really fresh fish!')
-addMarker(48.802200,2.129680,'Versailles','Not too far from Paris. Lots of gold')
-addMarker(23.810331,90.412521,'Dhaka','This is where my parents grew up.')
-addMarker(40.712776,-74.005974,'New York City','One of my first trips with my friends. Was unable to explore this city in 5 days.')
-addMarker(45.464203,9.189982,'Milan','Got stranded at the train station here because my flight prior was delayed :(')
-addMarker(43.769562,11.255814,'Florence','Beautiful city. However, I shouldve explored the city on a moped.')
-addMarker(41.902782,12.496365,'Rome','I\'m positive this city worse drivers than LA.')
-addMarker(41.902916,12.453389,'The Vatican City','I thought this would be bigger, but its literally just the Pope\'s bacislla')
